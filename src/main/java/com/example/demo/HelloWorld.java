e1fb38df08ca84bb80e45b66b03c45a1f326431d
wittawat16 <army@pirom.lo>
2018-09-02 23:36:12 +0700

    add post method

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,9 +1,8 @@
 package com.example.demo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -13,10 +12,18 @@ public class HelloWorld {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    @PostMapping("/greeting")
+    public Greeting postGreeting(@RequestBody String name) {
+
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+
     }
 
 }
